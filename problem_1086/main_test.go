9f9ed128ec513a822aed05f7344ccfcfd363c785
aldebap <aldeba_bla@yahoo.com>
2022-04-21 00:15:22 -0300

    Added an extra easy sodoku to the test

--- problem_1086/main_test.go
+++ problem_1086/main_test.go
@@ -19,11 +19,13 @@ func TestSodokuSolver(t *testing.T) {
 
 	t.Run(">>> Sodoku Solver", func(t *testing.T) {
 
-		//	a few test cases
-		var testScenarios = []struct {
+		type testData struct {
 			sodokuGame sodoku
 			result     bool
-		}{
+		}
+		//	a few test cases
+		var testScenarios []testData = []testData{
+			//	two easy level sodokus
 			{sodokuGame: sodoku{
 				{3, 0, 2, 0, 8, 0, 1, 0, 5},
 				{0, 0, 7, 2, 0, 0, 0, 6, 0},
@@ -34,7 +36,20 @@ func TestSodokuSolver(t *testing.T) {
 				{0, 2, 0, 3, 4, 5, 0, 0, 1},
 				{0, 0, 0, 0, 2, 6, 0, 0, 9},
 				{0, 0, 0, 0, 9, 0, 5, 2, 6},
-			}, result: true},
+			}, result: true,
+			},
+			{sodokuGame: sodoku{
+				{0, 0, 4, 0, 0, 7, 8, 3, 0},
+				{0, 1, 0, 9, 3, 0, 7, 0, 5},
+				{3, 5, 7, 0, 0, 4, 1, 0, 0},
+				{0, 3, 0, 0, 0, 2, 9, 8, 4},
+				{0, 0, 0, 0, 8, 1, 0, 0, 0},
+				{0, 2, 0, 0, 0, 0, 6, 0, 0},
+				{4, 7, 3, 2, 0, 8, 0, 0, 1},
+				{2, 6, 0, 1, 0, 0, 0, 7, 8},
+				{5, 0, 1, 6, 0, 0, 4, 0, 0},
+			}, result: true,
+			},
 		}
 
 		for _, test := range testScenarios {
